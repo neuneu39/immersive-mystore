9ffadda41f54a00ce05611f91a2707763997359a
kan.akutsu <sakamotoshizuya@gmail.com>
2018-06-10 21:40:22 +0900

    update router.post

--- server/items.route.js
+++ server/items.route.js
@@ -33,12 +33,16 @@ router.post('/', async (req, res, next) => {
     // 接続に対してクエリを発行する
     // see: https://github.com/sidorares/node-mysql2
     //const [rows/* , fields */] = await connection.query('select id, name, price, image_url from items');
-    const [rows] = await connection.query(
-      'insert into `my_table_name` (`name`, `price`, `imageUrl`) values (?, ?, ?)'
-    );
+    // const [rows] = await connection.query(
+    //   'insert into `my_table_name` (`name`, `price`, `imageUrl`) values (?, ?, ?)'
+    // );
+    const queryInsert = 'INSERT INTO items (name, price, image_url) VALUES (?, ?, ?)';
+    const [result] = await connection.query(queryInsert, [name, price, imageUrl]);
+    console.log('result= ', result);
+    res.json(result.insertId);
     //INSERT INTO `my_table_name` (`name`, `price`) VALUES ('Item 1', 100);
     // 戻り値をjsonとしてレスポンスを返す
-    res.json(rows);
+    //res.json(rows);
   } catch (err) {
     // console.log('*** catch ***'); クエリをエラーにしてコメントを外すと出力される
     next(err);
@@ -50,7 +54,7 @@ router.post('/', async (req, res, next) => {
     }
   }
 
-  res.json({name, price, imageUrl, received: true});
+  //res.json({name, price, imageUrl, received: true});
 });
 
 module.exports = router;
